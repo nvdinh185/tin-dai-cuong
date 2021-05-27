bad83e34dc04d9cbf00830d42f4a11ed2347a768
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-27 13:47:33 +0700

    vidu

---
+++ NhapMon/vidu.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+	int a = 1234554321;
+	printf("Gia tri cua a = %d\n", a);
+	
+	int b = 12345654321;
+	printf("Gia tri cua b = %d", b);
+}
--- ham/tamgiac.cpp
+++ ham/tamgiac.cpp
@@ -1,59 +1,46 @@
 #include <stdio.h>
 
-int laTamGiac(int, int, int);
-int tamGiacCan(int, int, int);
-
 int laTamGiac(int a, int b, int c){
 	if(a>0 && b>0 && c>0 && a+b>c && a+c>b && b+c>a) return 1;
 	return 0;
 }
 
 int tamGiacCan(int a, int b, int c){
-	if (a==b || b==c || c==a){
-		return 1;
- 	}
+	if (a==b || b==c || c==a) return 1;
  	return 0;
 }
 
 int tamGiacDeu(int a, int b, int c){
-	if (a==b && b==c){
-		return 1;
- 	}
+	if (a==b && b==c) return 1;
  	return 0;
 }
 
 int tamGiacVuong(int a, int b, int c){
-	if (a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b){
-		return 1;
- 	}
+	if (a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b) return 1;
  	return 0;
 }
 
 
 int main(){
 	int a, b, c;
-	printf("nhap a=");
+	printf("nhap a = ");
 	scanf("%d", &a);
-	printf("nhap b=");
+	printf("nhap b = ");
 	scanf("%d", &b);
-	printf("nhap c=");
+	printf("nhap c = ");
 	scanf("%d", &c);
 
-	if(laTamGiac(a, b, c)==1){
+	if(laTamGiac(a, b, c)){
 
-		if(tamGiacDeu(a, b, c)==1) printf("Ba so vua nhap tao thanh tam giac deu!");
+		if(tamGiacDeu(a, b, c)) printf("Ba so vua nhap tao thanh tam giac deu!");
 
-		else if(tamGiacCan(a, b, c)==1) printf("Ba so vua nhap tao thanh tam giac can!");
+		else if(tamGiacCan(a, b, c)) printf("Ba so vua nhap tao thanh tam giac can!");
 
-		else if(tamGiacVuong(a, b, c)==1) printf("Ba so vua nhap tao thanh tam giac vuong!");
+		else if(tamGiacVuong(a, b, c)) printf("Ba so vua nhap tao thanh tam giac vuong!");
 
 		else printf("Ba so vua nhap tao thanh tam giac thuong!");
 
-	} else {
-
-		printf("Ba so vua nhap khong tao thanh tam giac!");
-
-	}
+	} else printf("Ba so vua nhap khong tao thanh tam giac!");
 	
 	
 }
