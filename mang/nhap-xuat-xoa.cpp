9bf0c65ec561d79e999a42fe6b3e11e13ec55bb6
nvdinh185 <nvdinh0766777123@gmail.com>
2021-06-25 17:02:07 +0700

    xoa ok

--- mang/nhap-xuat-xoa.cpp
+++ mang/nhap-xuat-xoa.cpp
@@ -15,17 +15,27 @@ void xuatMang(int a[], int n){
 	}
 }
 
-void xoaPhanTu(int a[], int n, int a){
+void xoaPhanTu(int a[], int n, int x){
+	int i;
 	for(i=0; i<n; i++){
-		printf("%5d", a[i]);
+		if(a[i]==x) break;
 	}
+	for(int j=i; i<n-1; i++){
+		a[j] = a[j+1];
+	}
+	a[n-1] = 0;
 }
 
 main(){
-	int a[100];
 	int n;
 	printf("Nhap n = ");
 	scanf("%d", &n);
+	int a[n];
 	nhapMang(a, n);
+	xuatMang(a, n);
+	int x;
+	printf("\n\nNhap phan tu muon xoa: ");
+	scanf("%d", &x);
+	xoaPhanTu(a, n, x);
 	xuatMang(a, n);
 }
